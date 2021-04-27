548f6cdfdd30029d125f6d56917fd9a16adff281
Linux Kyrios <malinowskim92@protonmail.com>
2021-04-27 21:57:42 +0200

    New file model.ts for angular app

---
+++ todo/src/app/model.ts
@@ -0,0 +1,8 @@
+var model = {
+    user: "Linux Kyrios",
+    items: [{action: "Check for new iMac and buy it", done: false},
+        { action: "Buy new computer for new Linux instance", done: false},
+        { action: "Start looking for a new car", done: false},
+        { action: "Remove ex number from telephone", done: false},
+        { action: "Buy the delicous Buenos Nachos Pizza", done: true}]
+};
