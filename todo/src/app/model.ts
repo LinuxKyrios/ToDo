2268073220548c056b08dcdf722581159cc74be7
Linux Kyrios <malinowskim92@protonmail.com>
2021-04-27 22:57:05 +0200

    Modified and upgraded model.ts file as previous version was totally wrong

--- todo/src/app/model.ts
+++ todo/src/app/model.ts
@@ -1,8 +1,23 @@
-var model = {
-    user: "Linux Kyrios",
-    items: [{action: "Check for new iMac and buy it", done: false},
-        { action: "Buy new computer for new Linux instance", done: false},
-        { action: "Start looking for a new car", done: false},
-        { action: "Remove ex number from telephone", done: false},
-        { action: "Buy the delicous Buenos Nachos Pizza", done: true}]
-};
+export class Model {
+    user;
+    items;
+
+    constructor() {
+        this.user = "Linux Kyrios";
+        this.items = [new TodoItem("Check for new iMac and buy it", false),
+                        new TodoItem("Buy new computer for new Linux instance", false),
+                        new TodoItem("Start looking for a new car", false),
+                        new TodoItem("Buy the delicous Buenos Nachos Pizza", true),
+                        new TodoItem("Remove ex number from telephone", false),]
+    }
+}
+
+export class TodoItem {
+    action;
+    done;
+
+    constructor(action, done) {
+        this.action = action;
+        this.done = done;
+    }
+}
